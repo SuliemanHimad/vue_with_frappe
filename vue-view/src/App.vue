e9910eccc80ea08916080ee876130778276fdaf9
SuliemanHimad <suleiman.m@yooltech.com>
2025-02-22 11:52:50 +0300

    updated some files during automation

--- vue-view/src/App.vue
+++ vue-view/src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="container mt-5">
-    <h1>hello world</h1>
-    <button class="btn btn-primary">click me</button>
+    <button @click="fetchTodos" class="btn btn-primary">Fetch Todos</button>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="todo in data" :key="todo.id">{{ todo.title }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { ref } from "vue";
 
-fetch("/api/v2/document/ToDo")
-  .then((data) => data.json())
-  .then((response) => {
-    console.log(response);
-    console.log("welcome dear to be developer");
-    console.log("are you ready to learn vue");
-  });
+const isLoading = ref(false);
+const data = ref([]);
+
+function fetchTodos() {
+  isLoading.value = true;
+  fetch("/api/v2/document/ToDo", {
+    credentials: "include",
+  })
+    .then((data) => data.json())
+    .then((response) => {
+      console.log(response);
+      data[response];
+      isLoading.value = false;
+    });
+}
 </script>
